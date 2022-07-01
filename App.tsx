158cd8294f7e4fcd7214e304cc01c3124e32d98f
Transformhub <hr@transformhub.com>
2022-07-01 15:55:31 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+  Alert,
+} from 'react-native';
+import NativeSampleModule from './js/NativeSampleModule';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <TouchableOpacity
+        onPress={() => {
+          const message = NativeSampleModule.getHelloWorld('Daniel', 'Friyia');
+          Alert.alert(message);
+        }}>
+        <Text style={{fontSize: 30, color: 'red'}}>Hello</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default App;
